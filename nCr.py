712b08164eb335b9e7b4bc5b4caeac7152870376
vivek-kr-jsr <66364303+vivek-kr-jsr@users.noreply.github.com>
2021-01-05 16:56:10 +0530

    Create nCr.py

---
+++ nCr.py
@@ -0,0 +1,21 @@
+#https://practice.geeksforgeeks.org/problems/ncr1019/1
+
+   def nCr(self,n, k):
+        C = [[0 for x in range(k+1)] for x in range(n+1)]
+        M = 1000000007
+    # Calculate value of Binomial
+    # Coefficient in bottom up manner
+        for i in range(n+1):
+            for j in range(min(i, k)+1):
+            # Base Cases
+                if j == 0 or j == i:
+                    C[i][j] = 1
+ 
+            # Calculate value using
+            # previously stored values
+                else:
+                    C[i][j] = (C[i-1][j-1] + C[i-1][j])%M
+        
+      
+ 
+        return  C[n][k]
